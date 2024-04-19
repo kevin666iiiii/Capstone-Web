a3a6d41fc44e71ce179ced6c67eb0c0933ce52e3
kevin666iiiii <jingdis@andrew.cmu.edu>
2024-04-19 00:55:32 -0400

    1

---
+++ js/detail.js
@@ -0,0 +1,136 @@
+//let viewId = null;
+let date = null;
+let sessionId = null;
+let tutor = null;
+
+$(document).ready(function () {
+    init();
+    renderData();
+});
+
+
+function getUrlParams() {
+    let url = window.location.href;
+    let obj = {};
+    let index = url.indexOf("?");
+    let params = url.substr(index + 1);
+    //console.log(params);
+    if (index != -1) {
+        // 有参数时
+        let parr = params.split("&");
+        for (let i of parr) {
+            // 遍历数组
+            let arr = i.split("=");
+            obj[arr[0]] = arr[1];
+        }
+    }
+    return obj;
+}
+
+function backPage() {
+    window.history.back();
+}
+
+function renderData() {
+    $.getJSON(`../json/detail_${sessionId}.json`, function (data) {
+        let communicationLevelInfo = data.communicationLevelInfo;
+        let instructionDeliveryInfo = data.instructionDeliveryInfo;
+        let techToolUsageInfo = data.techToolUsageInfo;
+        let socioEmotionalTeachingInfo = data.socioEmotionalTeachingInfo;
+        let feedbackInfo = data.feedbackInfo;
+
+        renderCardInfo('Communication Level', communicationLevelInfo);
+        renderCardInfo('Instruction Delivery', instructionDeliveryInfo);
+        renderCardInfo('Tech & Tool Usage', techToolUsageInfo);
+        renderCardInfo('Socio-Emotional Teach', socioEmotionalTeachingInfo);
+        renderCardInfo('Feedback', feedbackInfo);
+
+        renderTimeLine(data.timeline.communicationLevel);
+        renderTimeLine(data.timeline.instructionDelivery);
+        renderTimeLine(data.timeline.techToolUsage);
+        renderTimeLine(data.timeline.socioEmotionalTeaching);
+        renderTimeLine(data.timeline.feedback);
+        renderTimeText();
+    })
+}
+
+function renderCardInfo(cardTitle, data) {
+
+    let remarksHtmlContent = '';
+    for (let i = 0; i < data.remarks.length; i++) {
+        remarksHtmlContent += `<div>${data.remarks[i]}</div>`
+    }
+
+    let contentCardBgColor = 'content-card-grey';
+    let progressColor = 'progress-color-light-grey';
+    let progressBarColor = 'progress-bar-color-white';
+
+    // if (data.dos != null && data.dont != null) {
+    //     contentCardBgColor = 'content-card-white';
+    //     progressColor = 'progress-color-lighter-grey';
+    //     progressBarColor = 'progress-bar-color-grey';
+    // }
+
+    $("#cardContainer").append(`
+            <div class="content-card ${contentCardBgColor}">
+                <p class="content-card-title">${cardTitle}</p>
+                <p class="content-card-sub">${data.level}</p>
+
+                <div class="progress-container">
+                    <div class="progress progress-container ${progressColor}">
+                    
+                        <div class="progress-bar ${progressBarColor}" role="progressbar" style="width: ${data.percent}%"
+                             aria-valuenow="15" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <div class="progress-percent">
+                        ${data.percent}%
+                    </div>
+                </div>
+                
+                <div class="remarks-container">
+                    ${remarksHtmlContent}
+                </div>
+            </div>
+    `);
+
+}
+
+function renderTimeLine(data) {
+
+    let blockHtmlContent = "";
+    for (let i = 0; i <data.length; i++) {
+        // blockHtmlContent += `<div class="row-block"><div class="time-line-block block-color-${data[i]}"></div></div>`;
+         blockHtmlContent += `<div class="time-line-block block-color-${data[i]}"></div>`;
+
+    }
+
+    $("#timeLineContainer").append(`
+        <div class="row-block">
+            ${blockHtmlContent}
+        </div>
+    `);
+}
+
+function renderTimeText() {
+    let timeTextArr = ["00'","03'","06'","09'","12'","15'","18'","21'","24'","27'","30'","33'","36'","39'","42'","45'","48'","51'","54'","57'"];
+    let timeTextHtmlContent = "";
+    for (let j = 0; j <timeTextArr.length; j++) {
+        timeTextHtmlContent += `<label class="time-text" style="margin-right: 4.2px">${timeTextArr[j]}</label>`;
+    }
+    $("#timeLineContainer").append(`
+        <div>
+            ${timeTextHtmlContent}
+        </div>
+    `);
+}
+
+function init() {
+    //viewId = getUrlParams().viewId;
+    date = getUrlParams().date;
+    sessionId = getUrlParams().sessionId;
+    tutor = getUrlParams().tutor.replace("%20", " ");
+
+    $("#dateBadge").text(date)
+    $("#sessionIdBadge").text(sessionId)
+    $("#tutorBadge").text(tutor)
+}
---
+++ js/index.js
@@ -0,0 +1,172 @@
+let technicalIssuesHasChecked = true;
+let sessionDurationHasChecked = true;
+let rubriceRatingHasChecked = true;
+let tableData = [];
+
+$(document).ready(function () {
+    init();
+    getAndRenderListData();
+});
+
+
+function getAndRenderListData() {
+    $("#tableBody").empty();
+
+    $.getJSON('../json/list_first_page.json', function (dataList) {
+
+        tableData = dataList;
+
+        for (let i = 0; i < dataList.length; i++) {
+            let data = dataList[i];
+
+            $("#tableBody").append(`
+                <tr>
+                    <td>${data.sessionId}</td>
+                    <td>${data.tutor}</td>
+                    <td>${data.subject}</td>
+                    <td>${data.date}</td>
+                    <td>${data.duration}</td>
+                    <td id='riskAreasTag${i}'></td>
+                    <td>
+<!--                        <img class="btn-op" src="../img/btn_view.png" onclick='toDetailPage(${data.viewId})'/>-->
+                        <img class="btn-op" src="../img/btn_view.png" onclick='toDetailPage(${i})'/>
+                    </td>
+                </tr>
+            `);
+
+
+            if (data.riskAreas.length > 0) {
+                $(`#riskAreasTag${i}`).empty();
+            }
+
+            for (let j = 0; j < data.riskAreas.length; j++) {
+
+                let riskArea = data.riskAreas[j];
+                let riskAreaText = riskArea.text;
+                let riskAreaColor = riskArea.color;
+
+                if (riskAreaText.toLowerCase() === "low rubric rating" && rubriceRatingHasChecked) {
+                    $(`#riskAreasTag${i}`).append(`
+                        <span class='tag tag-light-${riskAreaColor}'>${riskAreaText}</span>
+                    `)
+                }
+
+                if (riskAreaText.toLowerCase() === "technical issues" && technicalIssuesHasChecked) {
+                    $(`#riskAreasTag${i}`).append(`
+                        <span class='tag tag-light-${riskAreaColor}'>${riskAreaText}</span>
+                    `)
+                }
+
+                if (riskAreaText.toLowerCase() === "short sessions" && sessionDurationHasChecked) {
+                    $(`#riskAreasTag${i}`).append(`
+                        <span class='tag tag-light-${riskAreaColor}'>${riskAreaText}</span>
+                    `)
+                }
+
+            }
+
+            if ($(`#riskAreasTag${i}`).children().length === 0) {
+                $(`#riskAreasTag${i}`).empty();
+                $(`#riskAreasTag${i}`).append('-');
+            }
+
+        }
+
+    });
+
+}
+
+function showConditionDialog() {
+    $("#technicalIssuesCheckbox").prop("checked", technicalIssuesHasChecked);
+    $("#sessionDurationCheckbox").prop("checked", sessionDurationHasChecked);
+    $("#rubriceRatingCheckbox").prop("checked", rubriceRatingHasChecked);
+    $('#myModal').modal();
+}
+
+function removeCondition(conditionName) {
+    if (conditionName == 'technicalIssues') {
+        technicalIssuesHasChecked = false;
+        //$("#technicalIssuesTag").css("display", "inline-block");
+        $("#technicalIssuesTag").css("display", "none");
+    }
+    if (conditionName == 'sessionDuration') {
+        sessionDurationHasChecked = false;
+        $("#sessionDurationTag").css("display", "none");
+
+    }
+    if (conditionName == 'rubriceRating') {
+        rubriceRatingHasChecked = false;
+        $("#rubriceRatingTag").css("display", "none");
+    }
+
+    getAndRenderListData();
+}
+
+function setCondition() {
+
+    let technicalIssuesChecked = $("#technicalIssuesCheckbox").get(0).checked;
+    let sessionDurationChecked = $("#sessionDurationCheckbox").get(0).checked;
+    let rubriceRatingChecked = $("#rubriceRatingCheckbox").get(0).checked;
+
+    technicalIssuesHasChecked = technicalIssuesChecked;
+    sessionDurationHasChecked = sessionDurationChecked
+    rubriceRatingHasChecked = rubriceRatingChecked;
+
+    $("#technicalIssuesTag").css("display", technicalIssuesHasChecked ? "inline-block" : "none");
+    $("#sessionDurationTag").css("display", sessionDurationHasChecked ? "inline-block" : "none");
+    $("#rubriceRatingTag").css("display", rubriceRatingHasChecked ? "inline-block" : "none");
+
+    $('#myModal').modal('hide');
+
+    getAndRenderListData();
+}
+
+function toDetailPage(index) {
+    let item = tableData[index];
+    //let viewId = item.viewId;
+    let tutor = item.tutor;
+    let sessionId = item.sessionId;
+    let date = item.date;
+
+    window.location.href = `./detail.html?&tutor=${tutor}&sessionId=${sessionId}&date=${date}`;
+}
+
+
+function init() {
+
+    $('#dlgAddOpenWindow').on('show.bs.modal', function (event) {
+        var Id = $("#Id").val();
+        var Name = $("#Name").val();
+        var modal = $(this);
+        modal.find('.modal-body input[id=hidId]').attr("value", Id);
+        modal.find('.modal-body input[id=Name]').attr("value", Name);
+    });
+    $('#dlgAddOpenWindow').on('hide.bs.modal', function (e) {
+    });
+
+
+    $('#datetimepicker1').datetimepicker({
+        format: 'YYYY-MM-DD',
+        autoclose: true,
+        viewMode: 'days',
+        icons: {
+            today: 'fa fa-caret-square-o-right',
+            clear: 'fa fa-trash-o',
+            close: 'fa fa-close'
+        },
+    });
+
+    $('#datetimepicker2').datetimepicker({
+        format: 'YYYY-MM-DD',
+        autoclose: true,
+        viewMode: 'days',
+        icons: {
+            today: 'fa fa-caret-square-o-right',
+            clear: 'fa fa-trash-o',
+            close: 'fa fa-close'
+        },
+    });
+
+}
+
+
